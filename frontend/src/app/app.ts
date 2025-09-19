62fe60d5377d6165b8ab84ac74ac580dffae3373
momoanoW <mona.acc+github@posteo.de>
2025-09-19 23:09:59 +0200

    App Layout Integration, flexbox für gesamte App, globales App CSS, responsiv

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
+import { Nav } from "./nav/nav";
+import { Subnav } from "./subnav/subnav";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Nav, Subnav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
