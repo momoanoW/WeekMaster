7db0eb3c42a854a55d4c814cb707c839f029b118
momoanoW <mona.acc+github@posteo.de>
2025-09-19 23:01:27 +0200

    SubNav Komponente für primären Button (Neue Aufgabe) und sekundäre Buttons (Filter, Sortieren) mit Hover. mmit flexbox-Layout.

---
+++ frontend/src/app/subnav/subnav.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subnav',
+  imports: [],
+  templateUrl: './subnav.html',
+  styleUrl: './subnav.css'
+})
+export class Subnav {
+
+}
