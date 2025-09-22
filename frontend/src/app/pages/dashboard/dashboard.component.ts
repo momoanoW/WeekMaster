7ea96dda4e10ec1f5cb4804f6774c8f18f7af006
momoanoW <mona.acc+github@posteo.de>
2025-09-22 03:09:07 +0200

    Button *+Neue Aufgabe* hinzugefügt und Click-Event vorbereitet, damit Dialog Fenster danach öffnen kann

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { TaskTableComponent } from '../../components/task-table/task-table.component'; // 1. Importieren
+import { TaskTableComponent } from '../../components/task-table/task-table.component'; // TaskTable importieren
+import { TaskDialogComponent } from '../../components/task-dialog/task-dialog.component'; // Dialog für neue Aufgabe importieren
+import { CommonModule } from '@angular/common'; // 2. CommonModule für @if importieren
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [TaskTableComponent], // 2. Hier bekannt machen
-  templateUrl: './dashboard.component.html', // 3. Hier verwenden
+  imports: [TaskTableComponent, TaskDialogComponent, CommonModule],
+  templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
+  // Dialog "Schalter", anfangs ist der Dialog geschlossen
+  isDialogOpen = false;
+
+  // Methode, um den Schalter umzulegen
+  openDialog(): void {
+  // Ändere den Wert der Eigenschaft 'isDialogOpen' von 'false' auf 'true'
+  this.isDialogOpen = true;
+}
 }
