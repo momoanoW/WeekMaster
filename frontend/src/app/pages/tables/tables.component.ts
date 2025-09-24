b6114232e6f52a24564ee84daf475bcbe555fcab
momoanoW <mona.acc+github@posteo.de>
2025-09-24 08:33:54 +0200

    jetzt auch task-table gelöscht weil abgelöst durch task-display. jetzt werden  grid und table schön zusammen gemanaged

--- frontend/src/app/pages/tables/tables.component.ts
+++ frontend/src/app/pages/tables/tables.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { TaskTableComponent } from '../../components/task-table/task-table.component';
+import { TaskDisplayComponent } from '../../components/task-display/task-display.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tables',
   standalone: true,
-  imports: [TaskTableComponent, CommonModule],
+  imports: [TaskDisplayComponent, CommonModule],
   templateUrl: './tables.component.html',
   styleUrl: './tables.component.css'
 })
